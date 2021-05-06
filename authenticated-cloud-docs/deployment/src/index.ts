6a50664df98841ae843ac2318c24906d14d9bb07
Patryk Laurent <patryk.laurent@dmgt.com>
2021-05-06 12:24:16 -0700

    orienting

--- authenticated-cloud-docs/deployment/src/index.ts
+++ authenticated-cloud-docs/deployment/src/index.ts
@@ -6,17 +6,17 @@ export class MyStaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps, env: string) {
     super(scope, id, props);
 
-    Tags.of(this).add('service', 'auth-doc-test');
-    Tags.of(this).add('name', 'auth-doc-test-website');
+    Tags.of(this).add('service', 'authed-docs-test');
+    Tags.of(this).add('name', 'authed-docs-test-website');
     Tags.of(this).add('environment', env);
     Tags.of(this).add('product-owner', 'rob.chandhok@dmgt.com');
 
     new MyStaticWebSite(this, id, {
       domainName: this.node.tryGetContext('domain'),
-      siteSubDomain: `${env}.authdoctest`,
+      siteSubDomain: `${env}.autheddocstest`,
     });
   }
 }
 
 const app = new cdk.App();
-new MyStaticSiteStack(app, 'authdoctestStaticSite-dev', { env: { region: 'us-east-1', account: '787198526106' } }, 'local-dev');
+new MyStaticSiteStack(app, 'autheddoctestStaticSite-dev', { env: { region: 'us-east-1', account: '787198526106' } }, 'local-dev');
