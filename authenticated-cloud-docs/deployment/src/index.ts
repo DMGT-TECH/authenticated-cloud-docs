253aecb2852f00a3068d68c89da2f4b55e0682e1
Patryk Laurent <patryk.laurent@dmgt.com>
2021-05-12 10:06:09 -0700

    enjoying type checking

--- authenticated-cloud-docs/deployment/src/index.ts
+++ authenticated-cloud-docs/deployment/src/index.ts
@@ -11,7 +11,9 @@ export class MyStaticWebsiteStack extends cdk.Stack {
     Tags.of(this).add('environment', env);
     Tags.of(this).add('product-owner', 'rob.chandhok@dmgt.com');
 
-    console.log("Publishing site to " + `${env}.${process.env.AUTHENTICATED_CLOUD_DOCS__HOSTED_ZONE_SUBDOMAIN}`)
+    let fullPublishedDomain = `${env}.${process.env.AUTHENTICATED_CLOUD_DOCS__HOSTED_ZONE_SUBDOMAIN}.${this.node.tryGetContext('domain')}`
+    fs.writeFileSync('full_published_domain.txt', fullPublishedDomain)
+
     new MyStaticWebsite(this, id, {
       domainName: this.node.tryGetContext('domain'),
       siteSubDomain: `${env}.${process.env.AUTHENTICATED_CLOUD_DOCS__HOSTED_ZONE_SUBDOMAIN}`,
@@ -23,6 +25,6 @@ const app = new cdk.App();
 
 let env_name = process.env.AUTHENTICATED_CLOUD_DOCS__MAIN_BRANCH_TOP_SUBDOMAIN; // e.g., "main", "dev", or "www"
 if (process.env.PULL_NUMBER && process.env.PULL_NUMBER !== "null") {
-  env_name = "pr" + process.env.PULL_NUMBER;
+  env_name = "pr" + (process.env.PULL_NUMBER || "");
 }
 new MyStaticWebsiteStack(app, 'authdocstest-dev', { env: { region: 'us-east-1', account: '787198526106' } }, env_name);
